99ad306772eac167b45af5b4792f8524aa939a8e
Felix Heck <hi@whotheheck.de>
2017-05-09 17:21:47 +0200

    fix scripts

--- build.sh
+++ build.sh
@@ -3,4 +3,4 @@
 docker build $@ -t car2godeveloper/dind-node-build-runner . && \
   NODE_VER=$(docker run --rm --privileged car2godeveloper/dind-node-build-runner node --version) && \
   IMAGE_TAG=${NODE_VER#"v"} && \
-  docker tag -f car2godeveloper/dind-node-build-runner car2godeveloper/dind-node-build-runner:$IMAGE_TAG
+  docker tag car2godeveloper/dind-node-build-runner car2godeveloper/dind-node-build-runner:$IMAGE_TAG
